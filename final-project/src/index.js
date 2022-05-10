fcbb9056901ca92978db6df7ab357768b457cd1e
z <beheshizade275@gmail.com>
2022-05-10 11:46:17 +0430

    fix:fixing conflicts

--- final-project/src/index.js
+++ final-project/src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+  <StrictMode>
+     <App />
+  </StrictMode>
+   
 );
 
 // If you want to start measuring performance in your app, pass a function
