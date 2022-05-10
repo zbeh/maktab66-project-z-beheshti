b6dc133ab381a8e713b0b8bf4df1fc958e63924c
z <beheshizade275@gmail.com>
2022-05-10 11:53:16 +0430

    fixing conflicts

--- final-project/src/index.js
+++ final-project/src/index.js
@@ -5,9 +5,8 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
+
      <App />
-  </StrictMode>
    
 );
 
