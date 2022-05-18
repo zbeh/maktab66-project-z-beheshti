1c127249c5afa3a157120f7ed57f8c37669f23bf
z <beheshizade275@gmail.com>
2022-05-18 23:57:24 +0430

    feat:creating the product page

---
+++ final-project/src/Components/User/sideBar/SideBar.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import sideBarStyles from './sideBarStyles.module.scss'
+export default function SideBar() {
+  const [cat,setCat] = useState()
+  const [sub,setSub] = useState()
+  useEffect(()=>{
+    axios.get('http://localhost:3002/category').then(res=>setCat(res.data)).catch(error=> console.log(error))
+    axios.get('http://localhost:3002/subCategory').then(res=>setSub(res.data)).catch(error=> console.log(error))
+  },[])
+  return (
+    // <div className='container'>
+         <aside className={`${sideBarStyles.sideContainer}`}>
+        <div className={sideBarStyles.title}>
+        {cat?.filter((e, i) => i === 0).map((option, i) => (<Link to={`/all-products/${1}`} key={option.id}>{option.name}</Link>))}
+          <div className={sideBarStyles.sub}>
+            {sub?.map(s=><p key={s.id}>{s.name}</p>)}
+          </div>
+
+          
+        </div>
+        <div className={sideBarStyles.title}>
+        {cat?.filter((e, i) => i === 1).map((option, i) => (<Link to={`/all-products/${2}`}key={option.id}>{option.name}</Link>))}
+        <div className={sideBarStyles.sub}>
+          {sub?.map(s=><p key={s.id}>{s.name}</p>)}
+        </div>
+          
+        </div>
+        
+      </aside>
+    // </div>
+     
+  )
+}
--- final-project/src/Layout/UserLayout.js
+++ final-project/src/Layout/UserLayout.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import { Header, Footer, SideBar  } from '../Components'
-export default function UserLayout({children}) {
-  const currentPath = window.location
-  const targetPath = "/all-products"
-  return (
-    <>
-      <Header />
-        {currentPath.pathname===targetPath ? <SideBar/> : null}
-        {children}
-      <Footer/>
-    </>
-  )
+import React from "react";
+import { Header, Footer, SideBar } from "../Components";
+export default function UserLayout(props) {
+  const { sidebar } = props;
+  if (sidebar) {
+    return (
+      <>
+        <Header />
+        <div className="d-flex container">
+          <SideBar />
+        {props.children}
+        </div>
+        <Footer />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Header />
+        {props.children}
+        <Footer />
+      </>
+    );
+  }
 }
