b90ba300981210c345641dde4a60202188d027f0
z <beheshizade275@gmail.com>
2022-05-18 09:20:11 +0430

    style:changing panel style

--- final-project/src/Pages/index.js
+++ final-project/src/Pages/index.js
@@ -4,11 +4,9 @@ import AllProducts from "./User/AllProducts"
 import Pay from './User/Pay'
 import Purchase from './User/Purchase'
 import ProductDetail from "./User/ProductDetail"
-// import Products from './Admin/Products'
 import Login from './Admin/Login/Login'
 import Products from './Admin/products/Products'
-// import Login from './Admin/Login'
-import Panel from "./Admin/Panel"
+import Panel from "./Admin/panel/Panel"
 import Quantity from "./Admin/Quantity/Quantity"
 import Orders from "./Admin/Orders/Orders"
 import ProtectedRoute from "./Admin/ProtectedRoute"
