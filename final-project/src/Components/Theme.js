443ddcc259e2d3a64fe5e24d2d804f0c75487d6d
z <beheshizade275@gmail.com>
2022-05-20 15:19:50 +0430

    style:adding some styles

--- final-project/src/Components/Theme.js
+++ final-project/src/Components/Theme.js
@@ -1,8 +1,10 @@
 import {createTheme} from '@mui/material/styles'
+import { faIR} from '@mui/material/locale';
 export  const theme = createTheme({
     typography: {
       fontFamily: [
         'main',
       ].join(','),
     },
+    faIR
 });
