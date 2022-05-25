f99c541bcf9af439631f762770f2be3c6c938dac
z <beheshizade275@gmail.com>
2022-05-25 23:46:06 +0430

    feat:adding slider to home and product pages

--- final-project/src/Pages/User/ProductDetail/ProductDetail.js
+++ final-project/src/Pages/User/ProductDetail/ProductDetail.js
@@ -4,6 +4,12 @@ import { useFetch } from "../../../Helper/USeFetch";
 import productDetailStyles from "./productDetailStyles.module.scss";
 import { Button, Typography, CircularProgress } from "@mui/material";
 import axios from "axios";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-flip";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { EffectFlip, Pagination, Navigation } from "swiper";
 
 export default function ProductDetail() {
   const { id } = useParams();
@@ -11,11 +17,15 @@ export default function ProductDetail() {
     `http://localhost:3002/products?id=${id}`
   );
   console.log(data);
-
+  let array = [];
+  {
+    array.push(data?.data[0]);
+  }
+  console.log(array);
   if (error) {
     return (
       <>
-        <Typography variant="body1">ERROR - Typography Body1</Typography>
+        <Typography variant="body1">خطایی پیش آمده</Typography>
       </>
     );
   }
@@ -32,9 +42,36 @@ export default function ProductDetail() {
           <>
             <div className="container">
               <div className={`${productDetailStyles.container} d-flex`}>
-                <img
-                  src={`http://localhost:3002/files/${data.data[0].images[0]}`}
-                />
+                <Swiper
+                  effect={"flip"}
+                  grabCursor={true}
+                  pagination={true}
+                  navigation={true}
+                  modules={[EffectFlip, Pagination, Navigation]}
+                  className={productDetailStyles.swiper}
+                >
+                  <SwiperSlide className={productDetailStyles.SwiperSlide}>
+                    <img
+                      src={`http://localhost:3002/files/${data.data[0].images[0]}`}
+                    />
+                  </SwiperSlide>
+                  <SwiperSlide className={productDetailStyles.SwiperSlide}>
+                    <img
+                      src={`http://localhost:3002/files/${data.data[0].images[1]}`}
+                    />
+                  </SwiperSlide>
+                  <SwiperSlide className={productDetailStyles.SwiperSlide}>
+                    <img
+                      src={`http://localhost:3002/files/${data.data[0].images[2]}`}
+                    />
+                  </SwiperSlide>
+                  <SwiperSlide className={productDetailStyles.SwiperSlide}>
+                    <img
+                      src={`http://localhost:3002/files/${data.data[0].images[3]}`}
+                    />
+                  </SwiperSlide>
+                </Swiper>
+
                 <div className={productDetailStyles.info}>
                   <p>{data.data[0].name}</p>
                   <p>{data.data[0].price}تومان</p>
@@ -44,7 +81,7 @@ export default function ProductDetail() {
                       name="count"
                       min="1"
                       max={`${data.data[0].count}`}
-                      placeholder='1'
+                      placeholder="1"
                     />
                     <Button className={productDetailStyles.btn}>
                       افزودن به سبد خرید
